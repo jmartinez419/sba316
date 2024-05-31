b309fecccd70b402f228c917631b89ab84f1dce4
jmartinez419 <justynmartinez419@gmail.com>
2024-05-31 16:27:52 -0400

    added a form submission for email and name

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 let row1 = document.querySelector('.gallery')
 let row2 = document.querySelector(".galleryrow2")
+let flipbutton = document.getElementById("flipButton")
 let spinbutton = document.getElementById("spinButton")
 
-
-function spinFish(e){
+function flipFish(e){
     console.log("hi",row1,row2)
     
-    row1.style.transform = "rotateX(360deg)";
-    row2.style.transform = "rotate(360deg)";
+    row1.style.transform = `rotateX(180deg)`;
+    row2.style.transform = "rotateX(180deg)";
+}
+
+flipbutton.addEventListener("click", flipFish)
+
+function spinFish(e){
+    row1.style.transorm = "rotateZ(180deg)"
+    row2.style.transform = "rotateZ(180deg)"
 }
 
 spinbutton.addEventListener("click", spinFish)
+
+
+let form = document.createElement('form');
+
+
+let inputName = document.createElement('input');
+inputName.type = 'text';
+inputName.name = 'userName';
+inputName.placeholder = 'Name';
+
+let inputEmail = document.createElement('input');
+inputEmail.type = 'email';
+inputEmail.name = 'userEmail';
+inputEmail.placeholder = 'Email';
+
+
+let buttonSubmit = document.createElement('input');
+buttonSubmit.type = 'submit';
+buttonSubmit.value = 'Submit';
+
+form.appendChild(inputName);
+form.appendChild(inputEmail);
+form.appendChild(buttonSubmit);
+
+document.body.appendChild(form);
+
